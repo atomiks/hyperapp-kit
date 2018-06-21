2b1d526a1b2b5615a701e21377bcc87f7e82ddfb
atomiks <cc.glows@gmail.com>
2018-06-21 17:47:34 +1000

    parcel-bundler -> parcel, add tilde alias

--- src/js/view.js
+++ src/js/view.js
@@ -1,9 +1,10 @@
 import { h } from "hyperapp"
 import Counter from "./components/Counter"
+import hyperappLogo from "~/img/hyperapp.svg"
 
 export default (state, actions) => (
   <div class="app">
-    <img class="hyperapp-logo" src={require("../img/hyperapp.svg")} />
+    <img class="hyperapp-logo" src={hyperappLogo} />
     <h1>Hyperapp Kit</h1>
     <Counter />
   </div>
